852f8b5b9493f6d9eca48d9c1fc3d83f5d714a15
paudefclasspy <77154387+paudefclasspy@users.noreply.github.com>
2021-03-10 21:32:30 +0100

    Update app.py

--- AudioATexto/app.py
+++ AudioATexto/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import speech_recognition as sr
-app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    app = Flask(__name__)
     transcript =""
     if request.method == 'POST':
         print("Data Recieved")
